69179e32234f5ab0926d8b6d9cc0e45986c622cc
Pat O'Connor <patoconnor43@gmail.com>
2020-12-28 17:49:23 -0700

    Async fetch issues and cache for completion

--- lua/octo/completion.lua
+++ lua/octo/completion.lua
@@ -1,52 +1,7 @@
-local gh = require("octo.gh")
 local format = string.format
 local api = vim.api
-local json = {
-  parse = vim.fn.json_decode,
-  stringify = vim.fn.json_encode
-}
 
 local M = {}
-
-local function get_url(url, params)
-  url = url .. "?foo=bar"
-  for k, v in pairs(params) do
-    url = format("%s&%s=%s", url, k, v)
-  end
-  return url
-end
-
-local function get_repo_issues(repo, params)
-  params = params or {}
-  local query_params = {
-    state = params.state or "open",
-    per_page = params.per_page or 50,
-    filter = params.filter,
-    labels = params.labels,
-    since = params.since
-  }
-
-  local query = get_url(format("repos/%s/issues", repo), query_params)
-  local body =
-    gh.run(
-    {
-      args = {"api", query},
-      mode = "sync"
-    }
-  )
-
-  local issues = json.parse(body)
-
-  -- TODO: filter out pull_requests (NOT WORKING)
-  vim.tbl_filter(
-    function(e)
-      return e.pull_request == nil
-    end,
-    issues
-  )
-
-  return issues
-end
 
 function M.issue_complete(findstart, base)
   -- :help complete-functions
@@ -80,8 +35,7 @@ function M.issue_complete(findstart, base)
       end
     else
       if vim.startswith(base, "#") then
-        local repo = api.nvim_buf_get_var(0, "repo")
-        local issues = get_repo_issues(repo)
+        local issues = api.nvim_buf_get_var(0, "issues") or {}
         for _, i in ipairs(issues) do
           if vim.startswith("#" .. tostring(i.number), base) then
             table.insert(
