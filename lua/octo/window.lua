3763998a2b61d2465102db8ee655e9fee752a56f
Alvaro Muñoz <alvaro@pwntester.com>
2021-03-09 15:24:48 +0100

    replace popup.nvim with custom window management

--- lua/octo/window.lua
+++ lua/octo/window.lua
@@ -1,4 +1,3 @@
-local popup = require "popup"
 local format = string.format
 local api = vim.api
 
@@ -25,78 +24,84 @@ function M.create_centered_float(opts)
       max_line = math.max(#line, max_line)
     end
   end
-
   -- calculate window height/width
   local width, height
   if max_line > 0 then
     -- pre-defined content
-    width = math.min(vim_width * 0.9, max_line)
+    width = math.min(vim_width * 0.9, max_line + 4)
     if opts.header then
-      height = math.min(vim_height, 2 + #opts.content)
+      height = math.min(vim_height, 4 + 2 + #opts.content)
     else
-      height = math.min(vim_height, #opts.content)
+      height = math.min(vim_height, 4 + #opts.content)
     end
   else
     width = math.floor(vim_width * opts.x_percent)
     height = math.floor(vim_height * opts.y_percent)
   end
-
   -- calculate offsets
-  local x_offset = (vim_width - width) / 2
-  local y_offset = (vim_height - height) / 2
-  local header_winid
-  local header_win_opts = {border = {}}
+  local x_offset = math.floor((vim_width - width) / 2)
+  local y_offset = math.floor((vim_height - height) / 2)
+
+  -- outer win (header + border)
+  local outer_winid, outer_bufnr
+  outer_bufnr = api.nvim_create_buf(false, true)
+  local outer = {}
+  table.insert(outer, format("┌%s┐", string.rep("─", width-2)))
   if opts.header then
-    header_winid, header_win_opts =
-      popup.create(
-      {opts.header},
-      {
-        moved = "non-existant",
-        line =y_offset,
-        col = x_offset,
-        minwidth = width,
-        maxheight = 2,
-        border = {1, 1, 0, 1},
-        borderchars = {"─", "│", "", "│", "┌", "┐", "┤", "├"},
-        padding = {0, 0, 0, 0}
-      }
-    )
-    api.nvim_buf_call(header_win_opts.border.bufnr, function()
-      vim.cmd("setlocal eventignore+=WinClosed,WinLeave")
-    end)
-    local header_bufnr = api.nvim_win_get_buf(header_winid)
-    api.nvim_buf_set_option(header_bufnr, "modifiable", false)
-  end
-  local winid =
-    popup.create(
-    opts.content or {},
-    {
-      moved = "non-existant",
-      line = opts.header and y_offset+2 or y_offset,
-      col = x_offset,
-      minwidth = width,
-      minheight = height,
-      border = {1, 1, 1, 1},
-      borderchars = opts.header and {"─", "│", "─", "│", "├", "┤", "┘", "└"} or {"─", "│", "─", "│", "┌", "┐", "┘", "└"},
-      padding = {0, 0, 0, 0}
-    }
-  )
-  local bufnr = api.nvim_win_get_buf(winid)
-  if header_winid then
-    vim.cmd(string.format(
-      "autocmd BufLeave,BufDelete <buffer=%d> ++nested ++once :lua require('octo.window').try_close_wins(%d, %d, %d)",
-      bufnr,
-      winid,
-      header_winid,
-      header_win_opts.border.win_id))
+    table.insert(outer, format("│ %s%s │", string.sub(opts.header, 1, width-4), string.rep(" ", width - 4 - #string.sub(opts.header, 1, width-4))))
+    table.insert(outer, format("├%s┤", string.rep("─", width-2)))
+    for i=1, height-4 do
+      table.insert(outer, format("│%s│", string.rep(" ", width-2)))
+    end
   else
-    vim.cmd(string.format(
-      "autocmd BufLeave,BufDelete <buffer=%d> ++nested ++once :lua require('octo.window').try_close_wins(%d)",
-      bufnr,
-      winid))
+    for i=i, height-2 do
+      table.insert(outer, format("│%s│", string.rep("─", width-2)))
+    end
   end
+  table.insert(outer, format("└%s┘", string.rep("─", width-2)))
+  api.nvim_buf_set_lines(outer_bufnr, 0, -1, false, outer)
+  outer_winid = api.nvim_open_win(outer_bufnr, false, {
+    relative = "editor",
+    row = y_offset,
+    col = x_offset,
+    width = width,
+    height = height,
+    focusable = false
+  })
+  api.nvim_buf_set_option(outer_bufnr, "modifiable", false)
+  api.nvim_win_set_option(outer_winid, "foldcolumn", "0")
+  api.nvim_win_set_option(outer_winid, "signcolumn", "no")
+  api.nvim_win_set_option(outer_winid, "number", false)
+  api.nvim_win_set_option(outer_winid, "relativenumber", false)
+
+  -- content win
+  local bufnr = api.nvim_create_buf(false, true)
+  api.nvim_buf_set_lines(bufnr, 0, -1, false, opts.content or {})
+  local winid = api.nvim_open_win(bufnr, true, {
+    relative = "editor",
+    row = opts.header and (y_offset+2+2) or (y_offset+2),
+    col = x_offset + 2,
+    width = width - 4,
+    height = opts.header and (height-4-2) or (height-4),
+    focusable = true
+  })
+  api.nvim_win_set_option(winid, "previewwindow", true)
+  api.nvim_win_set_option(winid, "foldcolumn", "0")
+  api.nvim_win_set_option(winid, "signcolumn", "no")
+  api.nvim_win_set_option(winid, "number", false)
+  api.nvim_win_set_option(winid, "relativenumber", false)
+
+  -- window binding
+  local aucmd = string.format(
+    "autocmd BufLeave,BufDelete <buffer=%d> :lua require('octo.window').try_close_wins(%d, %d)",
+    bufnr,
+    winid,
+    outer_winid)
+  vim.cmd(aucmd)
+
+  -- mappings
   local mapping_opts = {script = true, silent = true, noremap = true}
-  api.nvim_buf_set_keymap(bufnr, "n", "<C-c>", format("<cmd>lua require'octo.window'.try_close_wins(%d)<CR>", winid), mapping_opts)
+  api.nvim_buf_set_keymap(bufnr, "n", "<C-c>", format("<cmd>lua require'octo.window'.try_close_wins(%d, %d)<CR>", winid, outer_winid), mapping_opts)
 
   return winid, bufnr
 end
