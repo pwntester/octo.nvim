0a277e6125b31e303e4810160aae9c6f7d032e1d
Alvaro Muñoz Sanchez <alvaro@pwntester.com>
2021-07-16 23:31:24 +0200

    Remove create_pr_mutation pagination

--- lua/octo/graphql.lua
+++ lua/octo/graphql.lua
@@ -2663,11 +2663,7 @@ M.create_pr_mutation =
             }
           }
         }
-        timelineItems(first: 100, after: $endCursor) {
-          pageInfo {
-            hasNextPage
-            endCursor
-          }
+        timelineItems(first: 100) {
           nodes {
             __typename
             ... on LabeledEvent {
