1f109ff39f88199d50f969c0c5f615c3df283536
PG Herveou <pgherveou@gmail.com>
2022-01-19 08:18:27 +0100

    Ensure all files from PR are loaded
    When `gh` returns multiple pages, the plugins break cause the output is not a JSON file but the concatenation of multiple JSON files
    One way to break it down is to use the `--jq` argument that will return each JSON content on a different page.

--- lua/octo/reviews.lua
+++ lua/octo/reviews.lua
@@ -135,7 +135,7 @@ function Review:initiate()
 
   local url = string.format("repos/%s/pulls/%d/files", pr.repo, pr.number)
   gh.run {
-    args = { "api", "--paginate", url },
+    args = { 'api', '--paginate', url, '--jq', '.' },
     cb = function(output, stderr)
       if stderr and not utils.is_blank(stderr) then
         utils.notify(stderr, 2)
@@ -146,7 +146,16 @@ function Review:initiate()
           deleted = "D",
           renamed = "R",
         }
-        local results = vim.fn.json_decode(output)
+
+        local results = {}
+        local page_outputs = vim.split(output, '\n')
+        for _, text in ipairs(page_outputs) do
+          local page_result = vim.fn.json_decode(text)
+          for _, result in ipairs(page_result) do
+            table.insert(results, result)
+          end
+        end
+
         local files = {}
         for _, result in ipairs(results) do
           local entry = FileEntry:new {
