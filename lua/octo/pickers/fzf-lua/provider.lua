4fe99ccd04565b1c5eca6519ed74263e8fdb2001
Milo Gertjejansen <milo@milogert.com>
2023-06-23 08:52:02 -0400

    finalize provider

--- lua/octo/pickers/fzf-lua/provider.lua
+++ lua/octo/pickers/fzf-lua/provider.lua
@@ -1,31 +1,23 @@
-local utils = require "octo.utils"
-
 local M = {}
 
-function M.not_implemented()
-  utils.error "Not implemented yet"
-end
-
 M.picker = {
-  issues = require "octo.pickers.fzf-lua.pickers.issues",
-  prs = require "octo.pickers.fzf-lua.pickers.prs",
-  commits = require "octo.pickers.fzf-lua.pickers.commits",
+  actions = require "octo.pickers.fzf-lua.pickers.actions",
+  assigned_labels = require "octo.pickers.fzf-lua.pickers.assigned_labels",
+  assignees = require "octo.pickers.fzf-lua.pickers.assignees",
   changed_files = require "octo.pickers.fzf-lua.pickers.changed_files",
+  commits = require "octo.pickers.fzf-lua.pickers.commits",
+  gists = require "octo.pickers.fzf-lua.pickers.gists",
+  issue_templates = require "octo.pickers.fzf-lua.pickers.issue_templates",
+  issues = require "octo.pickers.fzf-lua.pickers.issues",
+  labels = require "octo.pickers.fzf-lua.pickers.labels",
+  pending_threads = require "octo.pickers.fzf-lua.pickers.pending_threads",
   project_cards = require "octo.pickers.fzf-lua.pickers.project_cards",
   project_columns = require "octo.pickers.fzf-lua.pickers.project_columns",
-
-  search = require "octo.pickers.fzf-lua.pickers.search",
-  users = require "octo.pickers.fzf-lua.pickers.users",
-  assignees = require "octo.pickers.fzf-lua.pickers.assignees",
-  labels = require "octo.pickers.fzf-lua.pickers.labels",
-  assigned_labels = require "octo.pickers.fzf-lua.pickers.assigned_labels",
+  prs = require "octo.pickers.fzf-lua.pickers.prs",
   repos = require "octo.pickers.fzf-lua.pickers.repos",
   review_commits = require "octo.pickers.fzf-lua.pickers.review_commits",
-
-  gists = require "octo.pickers.fzf-lua.pickers.gists",
-  pending_threads = require "octo.pickers.fzf-lua.pickers.pending_threads",
-  actions = require "octo.pickers.fzf-lua.pickers.actions",
-  issue_templates = require "octo.pickers.fzf-lua.pickers.issue_templates",
+  search = require "octo.pickers.fzf-lua.pickers.search",
+  users = require "octo.pickers.fzf-lua.pickers.users",
 }
 
 return M
