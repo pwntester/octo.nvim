3cd359762c76f7b7ebe82942dc6b0d6be030f367
legobt <6wbvkn0j@anonaddy.me>
2024-11-03 05:03:56 +0000

    feat: match patterns in ssh_aliases

--- lua/octo/utils.lua
+++ lua/octo/utils.lua
@@ -174,8 +174,8 @@ function M.parse_remote_url(url, aliases)
     repo = chunks[#chunks]
   end
 
-  if aliases[host] then
-    host = aliases[host]
+  for alias, rhost in pairs(aliases) do
+    host = host:gsub("^" .. alias .. "$", rhost, 1)
   end
   if not M.is_blank(host) and not M.is_blank(repo) then
     return {
