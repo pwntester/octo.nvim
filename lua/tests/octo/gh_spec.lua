485814591a43e858289dc40bd238b323aa51531c
Will Dean <wd60622@gmail.com>
2025-01-10 00:06:18 +0100

    create tests

---
+++ lua/tests/octo/gh_spec.lua
@@ -0,0 +1,54 @@
+local gh = require "octo.gh"
+local eq = assert.are.same
+
+describe("gh", function()
+  it("booleans", function()
+    local args = {}
+    local opts = {
+      slurp = false,
+      paginate = true,
+    }
+    gh.insert_args(args, opts)
+    local expected = {
+      "--paginate",
+    }
+    eq(args, expected)
+  end)
+  it("single-char", function()
+    local args = {}
+    local opts = {
+      F = {
+        query = "query",
+      },
+      f = {
+        foo = "bar",
+      },
+    }
+    gh.insert_args(args, opts)
+    local expected = {
+      "-F",
+      "query=query",
+      "-f",
+      "foo=bar",
+    }
+    eq(args, expected)
+  end)
+  it("list", function()
+    local args = {}
+    local opts = {
+      f = {
+        items = { "a", "b", "c" },
+      },
+    }
+    gh.insert_args(args, opts)
+    local expected = {
+      "-f",
+      "items[]=a",
+      "-f",
+      "items[]=b",
+      "-f",
+      "items[]=c",
+    }
+    eq(args, expected)
+  end)
+end)
