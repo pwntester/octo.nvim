9888a006ebe127f38a4ec6af6941ccba96697db5
Will Dean <wd60622@gmail.com>
2025-11-05 13:47:17 -0500

    add logins module

---
+++ lua/octo/logins.lua
@@ -0,0 +1,39 @@
+---Helpers and Aliases for various logins
+
+local utils = require "octo.utils"
+local config = require "octo.config"
+
+local M = {}
+
+local copilot_logins = { "copilot-swe-agent", "copilot-pull-request-reviewer" }
+
+---Formats author login for display
+---@param author? {login: string}
+---@return {login: string}
+M.format_author = function(author)
+  if author == nil or utils.is_blank(author) then
+    return { login = "ghost" }
+  end
+
+  if vim.tbl_contains(copilot_logins, author.login) then
+    return { login = "Copilot" }
+  end
+
+  return author
+end
+
+---Gets user icon based on login
+---@param login string
+---@return string icon
+M.get_user_icon = function(login)
+  local conf = config.values
+  if login == "ghost" then
+    return conf.ghost_icon
+  elseif login == "Copilot" then
+    return conf.copilot_icon
+  else
+    return conf.user_icon
+  end
+end
+
+return M
