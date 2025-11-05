9740b575daed0d703fbd25b6135d8acff0bf4151
Will Dean <wd60622@gmail.com>
2025-11-05 13:52:36 -0500

    attempt to fix typing

--- lua/octo/logins.lua
+++ lua/octo/logins.lua
@@ -9,7 +9,7 @@ local copilot_logins = { "copilot-swe-agent", "copilot-pull-request-reviewer" }
 
 ---Formats author login for display
 ---@param author? {login: string}
----@return {login: string}
+---@return { name?: string, login?: string, isViewer?: boolean }
 M.format_author = function(author)
   if author == nil or utils.is_blank(author) then
     return { login = "ghost" }
